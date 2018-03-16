30b8d3215d0a9ae8b1f3aa8212905c0d381a5cfc
Josh Breckenridge <bloo1987@gmail.com>
2018-03-16 11:13:05 -0400

    Updated query logic with DB views

--- vagrant/log_analysis.py
+++ vagrant/log_analysis.py
@@ -34,4 +34,28 @@ def db_query(query, db_name, user):
         conn.close()
 
 
-#print(db_query('select * from authors;', 'news', 'vagrant'))
+# Output results of query for the most popular articles of all time
+q1 = db_query('select * from view_select_popular_articles',
+              'news', 'vagrant')
+
+print('Most popular articles of all time:\n')
+for entry in q1:
+    print('{} - {} views'.format(entry[0], entry[1]))
+
+
+# Output results of query for most popular authors of all time
+q2 = db_query('select * from view_select_popular_authors',
+              'news', 'vagrant')
+
+print('\nMost popular authors of all time:\n')
+for entry in q2:
+    print('{} - {} views'.format(entry[0], entry[1]))
+
+
+# Output results of query for days with error % greather than 1
+q3 = db_query('select * from view_select_error_percent',
+              'news', 'vagrant')
+
+print('\nDays with error count over 1%\n')
+for entry in q3:
+    print('{} - {}%'.format(entry[0], entry[1]))
